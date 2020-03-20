e3030d21ccd5524174d9cc1b364616a5154105b4
gitkiselev <482003@mail.ru>
2020-03-20 08:09:43 +0300

    from lesson03

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const formSearch = document.querySelector('.form-search'),
     inputDateDepart = formSearch.querySelector('.input__date-depart');
 
     //Данные
-    //const citiesApi = 'http://api.travelpayouts.com/data/ru/cities.json',
-    const citiesApi = 'dataBase/cities.json',
+    const citiesApi = 'http://api.travelpayouts.com/data/ru/cities.json',
+    //const citiesApi = 'dataBase/cities.json',
         proxy = 'https://cors-anywhere.herokuapp.com/',
         API_KEY = '5d097f5a665744f279a6f08d9eb4549a',
         calendar = 'http://min-prices.aviasales.ru/calendar_preload';
 
-        const FROM = 'SVX' // код Екатеринбугра
-        const TO = 'KGD' // код Калининграда
-        const WHEN = '2020-05-25'; // на 25 мая 2020
-        const CALENDAR_PRELOAD = CALENDAR + `?origin=${FROM}&destination=${TO}&depart_date=${WHEN}&one_way=true` // строка запроса
+        // const FROM = 'SVX' // код Екатеринбугра
+        // const TO = 'KGD' // код Калининграда
+        // const WHEN = '2020-05-25'; // на 25 мая 2020
+        // const CALENDAR_PRELOAD = CALENDAR + `?origin=${FROM}&destination=${TO}&depart_date=${WHEN}&one_way=true` // строка запроса
 
         let city = [];
 
@@ -69,6 +69,30 @@ const formSearch = document.querySelector('.form-search'),
         });
     }
 
+    const renderCheapDay = (cheapTicket) => {
+        console.log(cheapTicket)
+    };
+
+    const renderCheapYear = (cheapTickets) => {
+        console.log(cheapTickets)
+    };
+
+
+    const renderCheap = (data, date) => {
+        const cheapTicketYear = JSON.parse(data).best_prices;
+        //console.log(cheapTicketYear);
+        const cheapTicketDay = cheapTicketYear.filter((item) => {
+            return item.depart_date === date;
+        })
+        //console.log(cheapTicketDay);
+        renderCheapDay(cheapTicketDay)
+        renderCheapYear(cheapTicketYear)
+    };
+
+
+
+    //Обработчики событий
+
     inputCitiesFrom.addEventListener('input', () => {
         showCity(inputCitiesFrom, dropdownCitiesFrom)
     });
@@ -80,15 +104,45 @@ const formSearch = document.querySelector('.form-search'),
     selectCity(dropdownCitiesFrom, inputCitiesFrom);
     selectCity(dropdownCitiesTo, inputCitiesTo);
 
+    formSearch.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const cityFrom = city.find(item => inputCitiesFrom.value === item.name);
+        const cityTo = city.find(item => inputCitiesTo.value === item.name);
+
+        const formData = {
+            from: cityFrom.code,
+            to: cityTo.code,
+            when: inputDateDepart.value
+        }
+
+        const requestData = `?depart_date=${formData.when}&origin=${formData.from}&destination=${formData.to}&one_way=true`;
+
+        // const requestData2 = '?depart_date=' + formData.when +
+        //     '&origin=' + formData.from +
+        //     '&destination=' + formData.to +
+        //     '&one_way=true';//API_KEY не нужен
+
+            //console.log(requestData)
+            getData(calendar + requestData, (response) => {//можно без proxy
+               // console.log(response)
+               renderCheap(response, formData.when);
+            });
+    });
+
     //Вызовы функций
 
-    getData(citiesApi, data => {
+    getData(proxy + citiesApi, data => {
         city = JSON.parse(data).filter(item => item.name);
     });
 
     // домашнее задание - выводит в консоль информацию о рейсе Екатеринбург - Калининград на 25 мая
-    getData(CALENDAR_PRELOAD, (data) => {
-        console.log(JSON.parse(data));
+    // getData(CALENDAR_PRELOAD, (data) => {
+    //     console.log(JSON.parse(data));
         
-    });
+    // });
+
+    // getData(proxy + calendar + '?depart_date=2020-05-25&origin=SVX&destination=KGD&one_way=true&token=' + API_KEY, (data) => {
+    //     const cheapTicket = JSON.parse(data).best_prices.filter(item => item.depart_date === '2020-05-29')
+    //     console.log(cheapTicket)
+    // });
 
